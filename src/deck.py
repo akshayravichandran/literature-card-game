f92637fd77056ea8b8171ef581df93fd66e286b3
Akshay Ravichandran <akshay.r1995@gmail.com>
2018-12-05 02:31:55 -0500

    Adding card and deck skeleton (#1)

---
+++ src/deck.py
@@ -0,0 +1,42 @@
+from constants import DeckType, Suit, HalfSuit, FaceValue
+from card import Card
+
+
+class Deck:
+    def __init__(self, deck_type):
+        self.cards = []
+        self.deck_type = deck_type
+        initializer = {
+            DeckType.STANDARD: self.initialize_as_standard_deck
+        }
+        initializer.get(deck_type)()
+
+    def initialize_as_standard_deck(self):
+        # Build a deck of 54 cards with 9 half-suits
+        for fv in FaceValue:
+            # RED JOKER, BLACK JOKER and all 8s form the JOKER half-suit
+            # RED and BLACK are defined as face values 0 and 1
+            if fv.value < 2 or fv.value == 8:
+                if fv.value == 8:
+                    for s in Suit:
+                        # ignore the JOKER suit (not to be confused with the JOKER half-suit)
+                        if s != Suit.JOKER:
+                            card = Card(fv, s, HalfSuit.JOKER)
+                            self.cards.append(card)
+                else:
+                    # JOKER suit cards
+                    card = Card(fv, Suit.JOKER, HalfSuit.JOKER)
+                    self.cards.append(card)
+            else:
+                if 2 <= fv.value <= 7:
+                    # LOWER half-suits
+                    for s in Suit:
+                        if s != Suit.JOKER:
+                            card = Card(fv, s, HalfSuit.LOWER)
+                            self.cards.append(card)
+                else:
+                    # UPPER half-suits
+                    for s in Suit:
+                        if s != Suit.JOKER:
+                            card = Card(fv, s, HalfSuit.HIGHER)
+                            self.cards.append(card)
---
+++ test/test_deck.py
@@ -0,0 +1,15 @@
+from deck import Deck
+from constants import DeckType
+import unittest
+
+
+class Tests(unittest.TestCase):
+    def test_standard_deck(self):
+        d = Deck(DeckType.STANDARD)
+        self.assertTrue(d)
+        self.assertTrue(d.cards)
+        self.assertEqual(len(d.cards), 54)
+
+
+if __name__ == '__main__':
+    unittest.main()
